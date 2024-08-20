8388ae037995c2eb8f1ffdc88c18aa9dd81542f0
João Sá <joao.vsaa90@gmail.com>
2024-08-20 17:29:46 -0300

    chore: alteração na inicialização de livro

--- livros-angular/src/app/livro.ts
+++ livros-angular/src/app/livro.ts
@@ -1,15 +1,9 @@
 export class Livro {
-    codigo: number;
-    titulo: string;
-    autor: string[];
-    editora: string;
-    ano: number;
-
-    constructor(codigo: number, titulo: string, autor: string[], editora: string, ano: number) {
-        this.codigo = codigo;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.editora = editora;
-        this.ano = ano;
-    }
+    constructor(
+        public codigo: number = 0,
+        public titulo: string = '',
+        public autores: string[] = [],
+        public editora: string = '',
+        public ano: number = 0
+    ) {}
 }
