e92a5edfafd800bea172e868370bd12d39595d1e
João Sá <joao.vsaa90@gmail.com>
2024-08-21 15:09:47 -0300

    styled: alterações no estilo do cabeçalho da página

--- livros-angular/src/app/app-routing.module.ts
+++ livros-angular/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { LivroDadosComponent } from './livro-dados/livro-dados.component';
 const routes: Routes = [
   { path: 'lista', component: LivroListaComponent },
   { path: 'dados', component: LivroDadosComponent },
-  { path: '', redirectTo: '/lista', pathMatch: 'full' } // Rota padrão redirecionando para "lista"
+  { path: '', redirectTo: '/lista', pathMatch: 'full' }
 ];
 
 @NgModule({
